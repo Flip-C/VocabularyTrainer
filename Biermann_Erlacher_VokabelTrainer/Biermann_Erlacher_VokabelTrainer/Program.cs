799e4a2f8e6fac3423647fa8af1d09d263ce4d81
Fabian Erlacher <S2010438059@fhooe.at>
2021-01-21 10:58:07 +0100

    test

---
+++ Biermann_Erlacher_VokabelTrainer/Biermann_Erlacher_VokabelTrainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Biermann_Erlacher_VokabelTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
